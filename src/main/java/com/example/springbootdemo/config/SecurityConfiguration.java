7ce494a0a95dde7aba366a0e2e5f3683fefdab86
taohui <htao@thoughtworks.com>
2022-03-12 12:42:26 +0800

    [taohui] disable csrf to enable post request

--- src/main/java/com/example/springbootdemo/config/SecurityConfiguration.java
+++ src/main/java/com/example/springbootdemo/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.springbootdemo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,6 +11,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
